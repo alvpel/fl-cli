029c44f23bf4c1a669f94c73ae059aabb6af517e
Alvaro <alvpel@gmail.com>
2024-11-01 11:48:02 +0100

    Removed console output when opening a link to improve experience on Alfred

--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -1,5 +1,5 @@
 export async function openUrl(url: string) {
-  console.log(`Opening: ${url}`);
+  // console.log(`Opening: ${url}`); removed for usage in Alfred
   const process = new Deno.Command("open", { args: [url] }).spawn();
   await process.status;
 }
